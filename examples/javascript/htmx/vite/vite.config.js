8b62e0495b3e145c455b21e3bb225e17948a7bb3
Raimondas Rimkevičius (aka MekDrop) <github@mekdrop.name>
2026-03-25 11:40:40 +0200

    feat: add JavaScript htmx Vite example

---
+++ examples/javascript/htmx/vite/vite.config.js
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+
+export default defineConfig({
+  base: './',
+  plugins: [
+    nodePolyfills({
+      globals: {
+        global: false,
+        process: false,
+        Buffer: false
+      }
+    })
+  ],
+  resolve: {
+    alias: {
+      '@jose.espana/docstream': '@jose.espana/docstream/dist/officeparser.browser.js'
+    }
+  },
+  build: {
+    target: 'es2020',
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        format: 'es'
+      }
+    }
+  },
+  server: {
+    port: 3000
+  }
+});
